6c4bdafb26968ad85ff39f3d150a2c8808f7fa9a
jeet1225 <jeet1225@gmail.com>
2023-08-12 15:04:18 +0800

    v1

--- frontend/js/yunlingoods_frontend.js
+++ frontend/js/yunlingoods_frontend.js
@@ -26,6 +26,34 @@ $(function() {
             $(".tab > .on").add($this).toggleClass("on");
         }
     });
+    
+    var owl = $(".kvpicout.owl-carousel");
+    owl.owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        dots: false/*,
+        responsive: {
+            0: {
+                items: 1,
+                dots: true
+            },
+            1250: {
+                items: 3,
+                dots: false
+            }
+        }*/
+    });
+    // Go to the next item
+    $('.kv .arrr').click(function() {
+        owl.trigger('next.owl.carousel');
+    })
+    // Go to the previous item
+    $('.kv .arrl').click(function() {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        owl.trigger('prev.owl.carousel', [300]);
+    })
 });
 
 function toggleSlideMenu(event) {
