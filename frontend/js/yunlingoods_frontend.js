0f6211c38a8c85ff737ca9bbc8fd825e536ae5f6
jeet1225 <jeet1225@gmail.com>
2023-08-12 21:47:02 +0800

    v1

--- frontend/js/yunlingoods_frontend.js
+++ frontend/js/yunlingoods_frontend.js
@@ -54,6 +54,25 @@ $(function() {
         // Parameters has to be in square bracket '[]'
         owl.trigger('prev.owl.carousel', [300]);
     })
+
+    $('#date_timepicker_start').datetimepicker({
+        format:'Y/m/d',
+        onShow:function( ct ){
+         this.setOptions({
+          maxDate:jQuery('#date_timepicker_end').val()?jQuery('#date_timepicker_end').val():false
+         })
+        },
+        timepicker:false
+    });
+    $('#date_timepicker_end').datetimepicker({
+        format:'Y/m/d',
+        onShow:function( ct ){
+         this.setOptions({
+          minDate:jQuery('#date_timepicker_start').val()?jQuery('#date_timepicker_start').val():false
+         })
+        },
+        timepicker:false
+    });
 });
 
 function toggleSlideMenu(event) {
